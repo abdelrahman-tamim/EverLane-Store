a4cf966d4a238a407eacd7b4ca7a3fa88467b81d
abdelrahman-tamim <boditamim2013@gmail.com>
2025-07-10 18:48:59 +0300

    revert

--- src/app/api/test/page.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export default function GET(_: NextRequest) {
-    console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
-    return NextResponse.json({ key: process.env.NEXTAUTH_URL ?? "" });
-}
