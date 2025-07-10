5743cabac112cd6340fa1563da926680d8431c01
abdelrahman-tamim <boditamim2013@gmail.com>
2025-07-10 18:36:48 +0300

    check the NEXTAUTH_URL

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-import { Domain } from "domain";
 import type { NextConfig } from "next";
 
+console.log("NEXTAUTH_URL ------- ",process.env.NEXTAUTH_URL)
 const nextConfig: NextConfig = {
  images:{
   domains:["i.imgur.com","placehold.co","firebasestorage.googleapis.com","raw.githubusercontent.com","csomagolasmenedzsment.info","th.bing.com","cdn.finnflare.com","1ql3wj128j.ufs.sh","nextjs-milestone.s3.us-east-1.amazonaws.com","cdn.dummyjson.com","lh3.googleusercontent.com","scontent-pmo1-1.xx.fbcdn.net"]
