a4984cd9fa6df6daa1891f4af75fa1a9179551ac
abdelrahman-tamim <boditamim2013@gmail.com>
2025-07-12 00:47:47 +0300

    cl7

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -57,7 +57,7 @@ export default function Cart() {
           ))}
         </div>
 
-        {/* Subtotal Section */}
+        
         <div className='mt-8 bg-white shadow-lg rounded-lg overflow-hidden'>
           <div className='flex flex-col p-4 md:p-6'>
             <div className='flex justify-between items-center px-4 md:px-10 w-full'>
